826ade6a22bc5b8436a9cb8b5bc2b71607e1b5ec
Saksham Varshney <sakshamvarshney43@gmail.com>
2026-05-22 12:27:46 +0530

    Set_main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
+import { AIProvider } from "./context/AIContext";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <AIProvider>
+          <App />
+        </AIProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
