cb081f97b503f642397846e5851bf1583182eb3a
Saksham Varshney <sakshamvarshney43@gmail.com>
2026-06-03 22:34:39 +0530

    remove debug logs

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -7,8 +7,6 @@ import { Pool } from "pg";
 
 const connectionString = process.env.DATABASE_URL as string;
 
-console.log("DATABASE_URL =", connectionString);
-
 const pool = new Pool({
   connectionString,
 });
